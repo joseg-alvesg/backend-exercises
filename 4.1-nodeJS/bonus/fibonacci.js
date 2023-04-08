bac94df93b23b756e38c8ef410e2d6a42889335f
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-08 15:19:35 -0300

    bonus 2

--- 4.1-nodeJS/bonus/fibonacci.js
+++ 4.1-nodeJS/bonus/fibonacci.js
@@ -1,33 +1,91 @@
-const readline = require('readline-sync');
+const readline = require("readline-sync");
 
 function main() {
-  const n = readline.questionInt('digite 1 ou 0: ')
+  const n = readline.questionInt("digite 1 ou 0: ");
 }
-
 
 function handleFibonacci(qty) {
   let a = 1;
   let b = 1;
 
   for (let n = qty; n >= 0; n -= 1) {
-    const temp = a
+    const temp = a;
     a += b;
-    b = temp
+    b = temp;
   }
-  console.log(b)
-  return b
-}
+  console.log(b);
+  return b;
+};
 
 function main() {
-  const n = readline.questionInt('Insira \'N\' valor: ');
+  const n = readline.questionInt("Insira 'N' valor: ");
 
   if (n <= 0) {
-    console.log('entre com numero maior que zero')
-    return
+    console.log("entre com numero maior que zero");
+    return;
+  };
+
+  console.log(`N: ${n}`);
+  handleFibonacci(n - 2);
+};
+// main();
+
+function parameters(a, b, c) {
+  return new Promise((resolve, reject) => {
+    if (typeof a === "number" && typeof b === "number" && typeof c === "number") {
+      const result =  ((a + b) * c);
+      if (result < 50) {
+        return reject(new Error('Valor muito baixo'))
+      }
+      resolve(result)
+    };
+    return reject(new Error('Informe apenas números'));
+  });
+};
+
+async function first() {
+  try {
+    const resolve = await parameters(10, 10, 10);
+    console.log(resolve);
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+first()
+
+async function secondResolve() {
+
+  try {
+
+  const resolve = await parameters(1, 1, 'a');
+
+  console.log(resolve);
+
+  } catch (error) {
+
+  console.log(error.message);
+
   }
 
-  console.log(`N: ${n}`)
-  handleFibonacci(n - 2)
 }
 
-main()
+secondResolve();
+
+
+async function thirdResolve() {
+
+  try {
+
+  const resolve = await parameters(1, 1, 1);
+
+  console.log(resolve);
+
+  } catch (error) {
+
+  console.log(error.message);
+
+  }
+
+}
+
+thirdResolve();
