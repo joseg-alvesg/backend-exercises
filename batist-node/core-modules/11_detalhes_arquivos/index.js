dc8715b91071b892a21863dbc601ecee73173977
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-10 11:45:21 -0300

    stats

--- batist-node/core-modules/11_detalhes_arquivos/index.js
+++ batist-node/core-modules/11_detalhes_arquivos/index.js
@@ -4,4 +4,9 @@ fs.stat('novoarquivo.txt',  (err, stats) => {
   if (err) {
     return console.log(err);
   }
+  console.log(stats.isFile());
+  console.log(stats.isDirectory());
+  console.log(stats.isSymbolicLink());
+  console.log(stats.ctime);
+  console.log(stats.size);
 })
