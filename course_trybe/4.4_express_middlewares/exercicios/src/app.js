e8dd4d2ad9c5cbb2946edcbeb2226e2f4a26641f
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-12 22:01:31 -0300

    exercicio

--- course_trybe/4.4_express_middlewares/exercicios/src/app.js
+++ course_trybe/4.4_express_middlewares/exercicios/src/app.js
@@ -1,11 +1,12 @@
 // app.js
 const express = require('express');
+const nameValidation = require('./middlewares/nameValidation')
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/activities', (req, res) => {
+app.post('/activities', nameValidation, (req, res) => {
   res.status(201).json({ message: "sucesso" })
 })
 
