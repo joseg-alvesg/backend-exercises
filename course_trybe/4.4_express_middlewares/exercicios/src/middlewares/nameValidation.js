18637144bad3861050b737ac747c3193623f2103
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-12 21:23:11 -0300

    exercicio

--- course_trybe/4.4_express_middlewares/exercicios/src/middlewares/nameValidation.js
+++ course_trybe/4.4_express_middlewares/exercicios/src/middlewares/nameValidation.js
@@ -1,3 +1,8 @@
 module.exports = (req, res, next) => {
-  
+  const {name} = req.body
+  if(!name){
+    res.status(400).json({ message: "O campo name é obrigatorio"})
+  } else {
+    next()
+  }
 }
