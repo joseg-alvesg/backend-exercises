16faadcdc6f7a7defd2091038395a1a1e4ee575f
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-13 15:23:11 -0300

    aula

--- course_trybe/4.5_express_mysql/trybecash-api/src/routes/peopleRoutes.js
+++ course_trybe/4.5_express_mysql/trybecash-api/src/routes/peopleRoutes.js
@@ -5,19 +5,19 @@ const peopleDB = require('../db/peopleDB')
 
 const router = express.Router();
 
-// router.post('/', async (req, res) => {
+router.post('/', async (req, res) => {
 
-//   const person = req.body;
+  const person = req.body;
 
-//   try {
-//     const [result] = await peopleDB.insert(person);
-//     res.status(201).json({
-//       message: `Pessoa cadastrada com sucesso com o id ${result.insertId}` });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json({ message: 'Ocorreu um erro ao cadastrar uma pessoa' });
-//   }
-// });
+  try {
+    const [result] = await peopleDB.insert(person);
+    res.status(201).json({
+      message: `Pessoa cadastrada com sucesso com o id ${result.insertId}` });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Ocorreu um erro ao cadastrar uma pessoa' });
+  }
+});
 
 router.get('/', async (_req, res) => {
   try {
