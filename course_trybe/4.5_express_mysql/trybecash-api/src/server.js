7f1edf2550fcd628a5705d0b0edede81350b9e0b
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-13 14:21:15 -0300

    aula

--- course_trybe/4.5_express_mysql/trybecash-api/src/server.js
+++ course_trybe/4.5_express_mysql/trybecash-api/src/server.js
@@ -8,8 +8,8 @@ app.listen(PORT, async () => {
   console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
 
   // O código abaixo é para testarmos a comunicação com o MySQL
-  const [result] = await connection.execute('SELECT 1');
+  const [result] = await connection.execute('SELECT * from people');
   if (result) {
-    console.log('MySQL connection OK');
+    console.log(result);
   }
-});
+}); 
