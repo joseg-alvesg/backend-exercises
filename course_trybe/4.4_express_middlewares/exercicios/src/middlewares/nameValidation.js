d2f4e33f7e869c40c0642d89a4ff2f9a608cbeb7
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-12 21:40:16 -0300

    exercicio

--- course_trybe/4.4_express_middlewares/exercicios/src/middlewares/nameValidation.js
+++ course_trybe/4.4_express_middlewares/exercicios/src/middlewares/nameValidation.js
@@ -2,7 +2,9 @@ module.exports = (req, res, next) => {
   const {name} = req.body
   if(!name){
     res.status(400).json({ message: "O campo name é obrigatorio"})
-  } else {
-    next()
   }
+  if(name.length < 4){
+    res.status(400).json({ message: "O campo name precisa ter pelo menos 4 caracteres"})
+  }
+  next()
 }
